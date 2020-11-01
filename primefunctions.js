7e9f6f31baeb30e6bb0188de8173e149e7cac526
elliott.long <elliott.long@wsu.edu>
2020-11-01 14:49:18 -0800

    Project Built, Sieve of Eratosthenes Implemented

---
+++ primefunctions.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+let _ = require('underscore');
+
+function primeGen(limit) {
+  let array = [limit]; // create array of correct size
+  let upBound = Math.sqrt(limit); // create a binding to lower operation time
+  let output = []; // create array to hold output
+  for (let i = 0; i < limit; i++) { // activate all array members
+    array.push(true);
+  }
+  for (let i = 2; i <= upBound; i++) { // for 2 through sqrt limit
+    if (array[i] === true) { // if array member exists
+      for (let j = i * i; j < limit; j += i) { // for square of i until the limit
+        array[j] = false; // set multiples to false (not prime)
+      }
+    }
+  }
+
+  for (let i = 2; i < limit; i++) {
+    if (array[i] === true) {
+      output.push(i);
+    }
+  }
+  return output;
+}
+console.log(primeGen(100));
