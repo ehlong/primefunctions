c3e49528dd096bac552d44f88604428457a603b8
elliott.long <elliott.long@wsu.edu>
2020-11-01 18:19:50 -0800

    maxPrimeSum edited
    Implemented an easier to understand design utilizing
    underscore.

--- primefunctions.js
+++ primefunctions.js
@@ -3,41 +3,37 @@ const _ = require('underscore'); // silencing error due to IntelliJ not seeing U
 // feel free to delete initial comment if IntelliJ chooses to play nice
 
 function primeGen(limit) { // return list of prime numbers up to the limit
-  const array = [limit]; // create array of correct size
+  let array = [limit]; // create array of correct size
   const upBound = Math.sqrt(limit); // create a binding to lower operation time
-  const output = []; // create array to hold output
-  for (let i = 0; i < limit; i++) { // activate all array members
-    array.push(true);
+  for (let i = 1; i < limit; i++) { // activate all array members
+    array.push(i);
   }
-  for (let i = 2; i <= upBound; i++) { // for 2 through sqrt limit
-    if (array[i] === true) { // if array member exists
-      for (let j = i * i; j < limit; j += i) { // for square of i until the limit (orig. values wont get trimmed)
-        array[j] = false; // set multiples to false (not prime)
-      }
+  array.sort(function (a, b) { return a - b; }); // sort array
+  array = _.rest(array); // remove 1
+  for (let i = 2; i <= upBound; i++) { // run through list
+    for (let j = i * i; j <= limit; j += i) { // for square of i until the limit
+      array = _.reject(array, function (num) { return num === j; }); // trim out multiples
     }
   }
-  for (let i = 2; i < limit; i++) { // for each number
-    if (array[i] === true) { // if prime
-      output.push(i); // push to output
-    }
-  }
-  return output; // method used is more complicated but much faster than _.reject method
+  return array;
 }
 
 function cumulativeSum(list) { // produce a cumulative sum for list passed in
-  const retArray = [list.length]; // array to output
+  const arr = [list.length]; // array to output
   const first = list[0];
-  retArray[0] = first; // loaded with initial value
+  arr[0] = first; // loaded with initial value
   for (let i = 1; i < list.length; i++) {
     const hold = list[i]; //
-    retArray[i] = retArray[i - 1] + hold;
+    arr[i] = arr[i - 1] + hold;
   }
-  return retArray;
+  return arr;
 }
 
 function maxPrimeSum(limit) {
   let primes = primeGen(limit); // list of prime numbers to limit
-  let primeSums = _.reject(cumulativeSum(primes), function (num) { return num >= limit; });
+  let primeSums = _.reject(cumulativeSum(primes), function (num) {
+    return num >= limit;
+  });
   // list of sequential sums of primes, trimmed to max at limit
   let hold = primeSums.length; // a way to hold a length while manipulating
   const arr = []; // what will be returned
@@ -49,10 +45,15 @@ function maxPrimeSum(limit) {
       primeSums = _.initial(primeSums); // trim the end off
     }
     primes = _.rest(primes); // trim the beginning off
-    primeSums = _.reject(cumulativeSum(primes), function (num) { return num >= limit; });
+    primeSums = _.reject(cumulativeSum(primes), function (num) {
+      return num >= limit;
+    });
     // update prime sums from trimmed list
     hold--; // decrement hold
   }
-  return _.max(arr, function (ar) { return ar[1]; }); // return whatever has the highest consecutive list
+  return _.max(arr, function (ar) {
+    return ar[1];
+  }); // return whatever has the highest consecutive list
 }
-console.log(maxPrimeSum(100)); // added to remove ESLint notice
+
+console.log(maxPrimeSum(1000)); // added to remove ESLint notice
